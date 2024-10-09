438bdf929952bec10067b214f2e54f0ead281206
Kangue Loic <kangueloic9@gmail.com>
2024-10-09 10:52:17 +0100

    Modification brought to the pipe's date format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { ContactComponent } from './shared/contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/candidature-page/candidature-page.component.ts
+++ src/app/candidature-page/candidature-page.component.ts
@@ -3,14 +3,41 @@ import { HeaderComponent } from '../shared/header/header.component';
 import { NavigationComponent } from '../shared/navigation/navigation.component';
 import { FooterComponent } from '../shared/footer/footer.component';
 import { ContactComponent } from '../shared/contact/contact.component';
+import { CommonModule } from '@angular/common';
+
+type Offres = {
+  id : number;
+  typeDeContrat : string;
+  titre : string;
+  localisation : string;
+}
 
 @Component({
   selector: 'app-candidature-page',
   standalone: true,
-  imports: [HeaderComponent, NavigationComponent, FooterComponent, ContactComponent],
+  imports: [HeaderComponent, NavigationComponent, FooterComponent, ContactComponent, CommonModule],
   templateUrl: './candidature-page.component.html',
   styleUrl: './candidature-page.component.css'
 })
 export class CandidaturePageComponent {
-
+  offresSimilaires: Offres[] = [
+    {
+      id : 1,
+      typeDeContrat : "CDD",
+      titre : 'International Marketing Project Maager H/F',
+      localisation : 'Brazzaville',
+    },
+    {
+      id : 2,
+      typeDeContrat : "CDI",
+      titre : 'Contrôleur de Gestion Sociale H/F',
+      localisation : 'Pointe Noir',
+    },
+    {
+      id : 3,
+      typeDeContrat : "CDD",
+      titre : 'Conseiller Clientèle BtoB, H/F, CDD',
+      localisation : 'Pointe Noir',
+    }
+  ] 
 }
